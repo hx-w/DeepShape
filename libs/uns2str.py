ecc8072022c26ba1736d7682439b52c12bd2aaa4
hx-w <herixth@163.com>
2022-07-14 11:54:05 +0800

    save demo

--- libs/uns2str.py
+++ libs/uns2str.py
@@ -5,11 +5,26 @@
 
 import trimesh
 
-mesh = trimesh.load_mesh('static/unsmesh/simple_tooth.obj')
-
 class Uns2Str:
     '''
     Use parameterization method
     '''
     def __init__(self) -> None:
+        self._uns_mesh = trimesh.Trimesh()
+        self._str_mesh = trimesh.Trimesh()
         pass
+
+    def read(self, path: str) -> bool:
+        try:
+            self._uns_mesh = trimesh.load_mesh(path)
+            return True
+        except:
+            return False
+
+    def save(self, path: str) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    mtd = Uns2Str()
+    mtd.read('static/unsmesh/simple_tooth.obj')
